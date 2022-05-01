21110fc6c3397a6a4c6558ecae24c4aecd17a90e
Keviana Brailsford-Mickens <99054758+bkeviana@users.noreply.github.com>
2022-05-01 01:34:51 -0400

    questions for quiz

---
+++ questions.js
@@ -0,0 +1,64 @@
+
+
+let questions = [
+    {
+        numb: 1,
+        question: "What can JavaScript do?",
+        answer: "ALL OF THE ABOVE",
+        options: [
+          "change HTML styling such as CSS.",
+          "hides HTML elements",
+          "show HTML elements",
+          "ALL OF THE ABOVE"
+        ]
+
+    
+ 
+    },
+     {
+         numb: 2,
+         question: "Which is correct about the types of primitive data types?",
+         answer: "null,undefined,string,boolean,number",
+         options: [
+             "number,boolean,console.log,trim()", 
+         "null,undefined,string,boolean,number", 
+         "var,const,let,nan",
+          "concatenation,string methods,index0f,loops"]
+        
+     },
+     {
+         numb: 3,
+         question: "What does DOM stand for?",
+         answer: "Document Object Model",
+         options: [
+             "Dominic",
+          "Dominion Energy", 
+          "Document Object Model", 
+          "None is correct."]
+         
+     },
+     {
+         numb: 4,
+         question: "Which way is the best practice to write a variable?",
+         answer: "endGame",
+         options: [
+        "endGame",
+          "end_game",
+           "endgame", 
+           "ENDGAME"]
+      
+     },
+     {
+         numb: 5,
+         question: "What is incorrect about the rules of writing a variable?",
+         answer: "Can begin with any number",
+         options: [ 
+        "Can begin with any number",
+          "Must have a descriptve meaning",
+           "Can't use preserved words by JavaScript", 
+           "Must begin with a letter,with exceptions of '$' and '_'"]
+        
+     },
+ ];
+
+ 
